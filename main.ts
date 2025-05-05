a1835dd15ef04672061402f6f0a54b43d87376e5
cheuyin <yinstanleycheung@gmail.com>
2025-05-05 11:06:18 -0700

    Insert Created field when new note is created

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
-import { Plugin } from "obsidian";
+import { Plugin, TFile, TFolder } from "obsidian";
 export default class AutoDater extends Plugin {
 	async onload() {
-		// Without onLayoutReady callback, this even is triggered for every file when the vault first loads, which is undesired
+		// Without the onLayoutReady callback, this event is triggered for every existing file when the vault first loads, which is undesired
 		this.app.workspace.onLayoutReady(() =>
 			this.registerEvent(
-				this.app.vault.on("create", () => {
-					console.log("a new file has entered the arena");
+				this.app.vault.on("create", async (file) => {
+					if (file == null || file instanceof TFolder) return;
+					const tfile = file as TFile;
+					const createdField = `Created: ${this.getCurrentLocalDate()}`;
+					this.app.vault.modify(tfile, `---\n${createdField}\n---`);
 				})
 			)
 		);
+	}
+
+	getCurrentLocalDate(): string {
+		let currentDate = new Date();
+		const offset = currentDate.getTimezoneOffset();
+		currentDate = new Date(currentDate.getTime() - (offset * 60 * 1000));
+		return currentDate.toISOString().split("T")[0];
 	}
 }
