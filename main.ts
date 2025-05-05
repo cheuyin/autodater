cbdeecdb372a6649238d12707aaf4b9f7caf724c
cheuyin <yinstanleycheung@gmail.com>
2025-05-05 14:35:10 -0700

    Create/update Updated field when a note is modified

--- main.ts
+++ main.ts
@@ -1,17 +1,39 @@
-import { Plugin, TFile, TFolder } from "obsidian";
+import { Plugin, TFile } from "obsidian";
 export default class AutoDater extends Plugin {
 	async onload() {
 		// Without the onLayoutReady callback, this event is triggered for every existing file when the vault first loads, which is undesired
 		this.app.workspace.onLayoutReady(() =>
 			this.registerEvent(
-				this.app.vault.on("create", async (file) => {
-					if (file == null || file instanceof TFolder) return;
-					const tfile = file as TFile;
-					const currLocalDate = this.getCurrentLocalDate();
-					const initialFields = `Created: ${currLocalDate}\nUpdated: ${currLocalDate}`;
-					this.app.vault.modify(tfile, `---\n${initialFields}\n---`);
+				this.app.vault.on("create", (file) => {
+					if (file instanceof TFile) {
+						const initialFields = `Created: ${this.getCurrentLocalDate()}`;
+						this.app.vault.modify(
+							file,
+							`---\n${initialFields}\n---`
+						);
+					}
 				})
 			)
+		);
+
+		this.registerEvent(
+			this.app.vault.on("modify", (file) => {
+				if (file instanceof TFile) {
+					console.log(`${file.name} has been modified`);
+					try {
+						this.app.fileManager.processFrontMatter(
+							file,
+							(frontmatter) => {
+								frontmatter["Updated"] =
+									this.getCurrentLocalDate();
+								console.log("Metadata updated!")
+							}
+						);
+					} catch (error) {
+						console.log(error);
+					}
+				}
+			})
 		);
 	}
 
