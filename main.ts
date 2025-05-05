f6df5e88d005a5bb74d8e26e5fb834465cccd31a
cheuyin <yinstanleycheung@gmail.com>
2025-05-05 15:21:19 -0700

    Create the Created field using this.getCurrentLocalDate() method

--- main.ts
+++ main.ts
@@ -6,11 +6,17 @@ export default class AutoDater extends Plugin {
 			this.registerEvent(
 				this.app.vault.on("create", (file) => {
 					if (file instanceof TFile) {
-						const initialFields = `Created: ${this.getCurrentLocalDate()}`;
-						this.app.vault.modify(
-							file,
-							`---\n${initialFields}\n---`
-						);
+						try {
+							this.app.fileManager.processFrontMatter(
+								file,
+								(frontmatter) => {
+									frontmatter["Created"] =
+										this.getCurrentLocalDate();
+								}
+							);
+						} catch (error) {
+							console.log(error);
+						}
 					}
 				})
 			)
@@ -19,14 +25,12 @@ export default class AutoDater extends Plugin {
 		this.registerEvent(
 			this.app.vault.on("modify", (file) => {
 				if (file instanceof TFile) {
-					console.log(`${file.name} has been modified`);
 					try {
 						this.app.fileManager.processFrontMatter(
 							file,
 							(frontmatter) => {
 								frontmatter["Updated"] =
 									this.getCurrentLocalDate();
-								console.log("Metadata updated!")
 							}
 						);
 					} catch (error) {
